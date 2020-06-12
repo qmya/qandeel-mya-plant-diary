bb846f6d2efc6b014bc292df777633ec90144105
qmya <qandeelgul90@gmail.com>
2020-06-11 22:06:12 -0400

    created the Link

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import "./Navigation.scss";
-// import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/flower.svg';
 
 function Navigation () {
     
     return ( 
         <header className="header">
-            {/* <NavLink to="/"> */}
+            <Link to="/">
              <img src ={Logo} className="header__icon" alt="logo for the plant site"/>
-            {/* </NavLink> */}
+            </Link>
             <h1 className="header__one">Plant Diary</h1>
             <nav>
             
-                {/* <NavLink to= "/MyPlants"  className="navigation__location"activeClassName="navigation__location--selected"> */}
+                <Link to= "/myplants">
                     <div className="header__two">My Plants</div> 
-                {/* </NavLink>  */}
+                </Link>
             </nav>
         </header>
     );
