fcded6fe4fe976e7317794742f3f1e3d93743e5d
qmya <qandeelgul90@gmail.com>
2020-06-11 22:01:05 -0400

    added video and style the opacity

--- client/src/components/MainVideo/MainVideo.js
+++ client/src/components/MainVideo/MainVideo.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import './MainVideo.scss';
-// import Image from "../../assets/hero-image.jpg";
+import plantVideo from "../../assets/plantvideo.mp4";
 
 function MainVideo () {
     return(
        <>
-        {/* //   <h1>A diary to keep your plant happy and healthy</h1>
-        //   <video loop="true" autoplay="autoplay" id="vid" muted>
-        //       <source src="http://thenewcode.com/assets/videos/polina.webm"></source>
-        //   </video> */}
-        {/* <img src={Image} alt="Main image"/>
-       <h1>helooo  lsjdda;lscdjhskjlkalxzcnbjhkjdsklamxzncjndlksa,mxz</h1> */}
-       {/* <div className="heroimage">
-       <h1 className="heroimage__paragraph">A diary to keep your plant happy and healthy</h1>
-       </div> */}
-       {/* <h1 class="heroimage">A diary to keep your plant happy and healthy</h1> */}
-      <div className="heroimage">
+       
+         
+           
+      
+      <div className="heroimage__container" >
+      <video className="heroimage" loop="true" autoPlay="autoplay" id="vid" muted>
+           <source src ={plantVideo}></source>
+           </video>
           <div className="heroimage__wrapper">
+          
            <h1 className="heroimage__paragraph">A diary to keep your plant happy and healthy</h1> 
           </div>
       </div>
