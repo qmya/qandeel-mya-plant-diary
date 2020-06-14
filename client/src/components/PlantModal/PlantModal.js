c1f4d8ef1e67595459613c3a17a793d8e856b579
qmya <qandeelgul90@gmail.com>
2020-06-14 16:34:59 -0400

    created a plant modal

--- client/src/components/PlantModal/PlantModal.js
+++ client/src/components/PlantModal/PlantModal.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './PlantModal.scss';
+import axios from 'axios';
 
 const PlantModal = (props) => {
-        
+    const [name, setName] = useState('') ;
+    const [nickName, setnickName] =useState('');
+    
+   const handleSaveClicked = (event) => {
+        event.preventDefault();
+    
+        const data = new FormData(event.target);
+       
+        axios
+            .post('http://localhost:8080/plant/userData', {
+                name:`${name}`,
+                nickName:`${nickName}`
+            })
+            .then(function (response) {
+                props.getData();
+                props.closeModal();
+              })
+              .catch(function (error) {
+                console.log(error);
+              })
+            //   alert('Add plant button was clicked');
+      }
     
 return (
       <div className="modal">
-        <h2>Add Plant</h2>
-        <form >
+        <h2 className="modal__heading">Add Plant</h2>
+        <form onSubmit={handleSaveClicked}>
                 <div className='modal__name'>
                     <label htmlFor='name' className='label'>Name</label>
-                    <input name='name' placeholder='Name' className='input'/>
+                    <input name='name' onChange={(e) => setName(e.target.value) } placeholder='Name' className='input'/>
                 </div>
                 <div className='modal__name'>
                     <label htmlFor='nickname' className='label'>Nick Name</label>
-                    <input name='Nick name' placeholder='Nick Name' className='input'/>
+                    <input name='nickName' onChange={(e) => setnickName(e.target.value) } placeholder='Nick Name' className='input'/>
                 </div>
                 <div className='modal__buttons'>
                     <button type='submit' className='modal__button'>Add PLANT</button>
-                    <button type='button' className='modal__button--cancel' onClick={props.closeModal}>CANCEL</button>
+                    <button type='button' className='modal__button button--cancel
+                    ' onClick={props.closeModal}>CANCEL</button>
                 </div>
         </form>
         
