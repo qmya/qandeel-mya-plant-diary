658252290e0e46ae01c9ce9540001ddb5bc70a68
qmya <qandeelgul90@gmail.com>
2020-06-14 16:36:41 -0400

    props the item from the backend

--- client/src/components/PlantList/PlantList.js
+++ client/src/components/PlantList/PlantList.js
@@ -4,24 +4,25 @@ import './PlantList.scss';
 
 const PlantList =(props) => {
     return(
-        <div>
+        <div className="plantlist">
             <div>
-                <h1>Your Plants</h1>
-                <div>
-                    <p>Below is the list of the plants you have added to your garden.</p>
-                    <button>ADD PLANTS</button>
-                </div>
-                {/* {props.PlantList.map(plant =>{
-                    return 
+                {/* <h1 className="plantlist__header">Your Plants</h1>
+                <div  className="plantlist__flex">
+                    <p className="plantlist__paragraph">Below is the list of the plants you have added to your garden.</p>
+                    <button className="plantlist__button">ADD PLANTS</button>
+                </div> */}
+                {props.plantList.length ? props.plantList.map(plant =>(
+                    
                     <PlantItem
                     id={plant.id}
                     image={plant.image}
                     name={plant.name}
+                    nickName={plant.nickName}
                     description={plant.description}
                     light={plant.light}
-                    // water-frequency={plant.water-frequency}
+                    water={plant.water}
                     />
-                })} */}
+                )): null}
                 
             </div>
             
