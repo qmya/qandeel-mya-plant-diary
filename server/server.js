ebf941cf2fc4660f2647e8cd2e8f215d6f831932
qmya <qandeelgul90@gmail.com>
2020-06-17 22:44:23 -0400

    created a user.json and also added more plants to the plant.json

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
+
 const cors = require('cors');
 const plant = require('./route/plant.js');
-
+const userdata =require('./route/user.js');
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/plant', plant);
-
+app.use('/userData', userdata);
 
 
 app.listen(8080, () => {
