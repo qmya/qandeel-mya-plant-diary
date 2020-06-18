84f4764b4a1bd411ae9666081b001029e6f810fa
qmya <qandeelgul90@gmail.com>
2020-06-17 22:37:37 -0400

    make the modal smaller

--- client/src/pages/MyPlants/MyPlants.js
+++ client/src/pages/MyPlants/MyPlants.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import ReactModal from "react-modal";
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
 import PlantList from "../../components/PlantList/PlantList.js";
 import PlantModal from "../../components/PlantModal/PlantModal.js";
 import Whoops from "../Whoops/Whoops.js";
@@ -7,13 +9,20 @@ import { withRenderCtrl } from "react-render-ctrl";
 import axios from "axios";
 import "./MyPlants.scss";
 
+ReactModal.setAppElement('body');
+
 const API_URL = "http://localhost:8080";
 
+
 class MyPlants extends React.Component {
-  state = {
+  constructor(props){
+    super(props)
+  this.state = {
     plantList: [],
     modalIsOpen: false,
+
   };
+}
 
   openModal = () => {
     this.setState({ modalIsOpen: true });
@@ -29,14 +38,30 @@ class MyPlants extends React.Component {
   }
 
   componentDidMount() {
-    axios.get("http://localhost:8080/plant").then((response) => {
+    axios.get(API_URL +'/plant').then((response) => {
       this.setState({ plantList: response.data });
+    
     });
   }
   getData = () => {
-    axios.get("http://localhost:8080/plant").then((response) => {
+    axios.get(API_URL +'/plant').then((response) => {
       this.setState({ plantList: response.data });
     });
+  }
+
+  
+   
+// added delete to delete plant when its no more
+  handleDelete = deleteID => {
+    
+    axios.delete(API_URL +'/userData/' +deleteID)
+       .then(response => {
+         this.setState({
+           plantList:response.data
+         })
+       })
+       .catch(error => console.log(`Error : ${error}`)
+       )
   }
 
   // componentDidUpdate(prevState){
@@ -73,9 +98,10 @@ class MyPlants extends React.Component {
         </div> */}
         {this.state.plantList.length > 0 ? (
           <div>
-            <PlantList plantList={this.state.plantList} openModal={this.changeModal}/>
-            <ReactModal isOpen={this.state.modalIsOpen}>
+            <PlantList plantList={this.state.plantList} openModal={this.changeModal} handleDelete={this.handleDelete} getData={this.getData}/>
+            <ReactModal className= "myplant__modal" isOpen={this.state.modalIsOpen}>
               <PlantModal
+                
                 closeModal={this.changeModal}
                 getData={this.getData}
               />
