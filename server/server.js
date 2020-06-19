1de59b2bcb012b8eae2d53777ace345bd9293bd5
qmya <qandeelgul90@gmail.com>
2020-06-19 11:24:12 -0400

    prettier the code

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
-const plant = require('./route/plant.js');
-const userdata =require('./route/user.js');
+const cors = require("cors");
+const plant = require("./route/plant.js");
+const userdata = require("./route/user.js");
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/plant', plant);
-app.use('/userData', userdata);
-
+app.use("/plant", plant);
+app.use("/userData", userdata);
 
 app.listen(8080, () => {
-  console.log('Listening on port 8080');
+  console.log("Listening on port 8080");
 });
