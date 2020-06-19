1d607e422f25cf44788d75a5ac1d03d986c51747
qmya <qandeelgul90@gmail.com>
2020-06-19 01:17:09 -0400

    remove the comments from the file

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import icon from './assets/icons8-linkedin.svg';
+// import icon from './assets/icons8-linkedin.svg';
 import Navigation from './components/Navigation/Navigation.js'
 import Home from './pages/Home/Home.js';
 import MyPlants from './pages/MyPlants/MyPlants.js';
-import Footer from './components/Footer/Footer.js';
+// import Footer from './components/Footer/Footer.js';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.scss';
 
@@ -16,12 +16,14 @@ function App() {
         <Route path='/' exact component ={Home} />;
         <Route path='/myplants' component = {MyPlants}/>
         </Switch>
-        <Footer />
-        {/* <footer className="footer">
+       
+        
+      </BrowserRouter>
+      {/* <Footer /> */}
+      {/* <footer className="footer">
          <h5>Qandeel Labs</h5>
         <a target="_blank" href="https://www.linkedin.com/in/qandeel-mya/"> <img className="icon" src={icon} alt ="linkedIn icon"/></a>
         </footer> */}
-      </BrowserRouter>
     </>
 
   ); 
