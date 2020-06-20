7bd3a3b0f5ae33a29bb29b243833c22869d4cf5f
qmya <qandeelgul90@gmail.com>
2020-06-19 20:58:32 -0400

    cleanup and prettier the code

--- client/src/components/MainVideo/MainVideo.js
+++ client/src/components/MainVideo/MainVideo.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import './MainVideo.scss';
+import React from "react";
+import "./MainVideo.scss";
 import plantVideo from "../../assets/plantvideo.mp4";
 
-function MainVideo () {
-    return(
-       <>
-       
-         
-           
-      
-      <div className="heroimage__container" >
-      <video className="heroimage" loop={true} autoPlay="autoplay" id="vid" muted>
-           <source src ={plantVideo}/>
-           </video>
-          <div className="heroimage__wrapper">
-          
-           <h1 className="heroimage__paragraph heroimage__animation">
-               A diary to keep your plant  happy and healthy</h1> 
-          </div>
+function MainVideo() {
+  return (
+    <>
+      <div className="heroimage__container">
+        <video
+          className="heroimage"
+          loop={true}
+          autoPlay="autoplay"
+          id="vid"
+          muted
+        >
+          <source src={plantVideo} />
+        </video>
+        <div className="heroimage__wrapper">
+          <h1 className="heroimage__paragraph heroimage__animation">
+            A diary to keep your plant happy and healthy
+          </h1>
+        </div>
       </div>
-       </>
-        
-    );
+    </>
+  );
 }
 
 export default MainVideo;
-
