02a65c8f5e8077ec2e28aec52a2f39497d0f7fa4
qmya <qandeelgul90@gmail.com>
2020-06-19 21:49:07 -0400

    cleanup and prettier the code

--- client/src/pages/Whoops/Whoops.js
+++ client/src/pages/Whoops/Whoops.js
@@ -1,59 +1,59 @@
-import React from 'react';
-import ReactModal from 'react-modal';
-import PlantModal from '../../components/PlantModal/PlantModal.js';
-import './Whoops.scss'
-// import Icon from '../../assets/linkedin.svg';
-
-import Illustration from '../../assets/illustration-two.svg';
+import React from "react";
+import ReactModal from "react-modal";
+import PlantModal from "../../components/PlantModal/PlantModal.js";
+import "./Whoops.scss";
+import Illustration from "../../assets/illustration-two.svg";
 
 class Whoops extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalIsOpen: false,
+    };
+  }
+  openModal = () => {
+    this.setState({ modalIsOpen: true });
+    console.log("open");
+  };
 
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
 
-   
-
-
-
-    constructor(props) {
-        super(props);
-        this.state = { 
-            modalIsOpen: false 
-        };
-      }
-    
-      openModal = () => {
-        this.setState({modalIsOpen: true});
-        console.log("open");
-      }
-    
-      closeModal = () => {
-        this.setState({modalIsOpen: false});
-      }
-    
-   
-    
-     
-
-
-
-
- render(){
-    return(
-        <div>
-            <div className="whoops">
-                <div className="whoops__wrap">
-                    <h1 className="whoops__header">Whoops,looks like you don't have any plants</h1>
-                    <p className="whoops__paragraph">Hit the "Add Plants" button to start adding to your list</p>
-                    <button type="button" className="whoops__button" onClick={this.openModal}>ADD PLANTS</button>
-                </div>
-                <img className="whoops__image" src={Illustration} alt=' empty plants page'/>
-            </div>
-        <ReactModal className="whoops__modal" isOpen={this.state.modalIsOpen}>
-            <PlantModal closeModal={this.closeModal} getData={this.props.getData}/>
-        </ReactModal>
-       
+  render() {
+    return (
+      <div>
+        <div className="whoops">
+          <div className="whoops__wrap">
+            <h1 className="whoops__header">
+              Whoops,looks like you don't have any plants
+            </h1>
+            <p className="whoops__paragraph">
+              Hit the "Add Plants" button to start adding to your list
+            </p>
+            <button
+              type="button"
+              className="whoops__button"
+              onClick={this.openModal}
+            >
+              ADD PLANTS
+            </button>
+          </div>
+          <img
+            className="whoops__image"
+            src={Illustration}
+            alt=" empty plants page"
+          />
         </div>
-    ); 
- }
+        <ReactModal className="whoops__modal" isOpen={this.state.modalIsOpen}>
+          <PlantModal
+            closeModal={this.closeModal}
+            getData={this.props.getData}
+          />
+        </ReactModal>
+      </div>
+    );
+  }
 }
 
 export default Whoops;
