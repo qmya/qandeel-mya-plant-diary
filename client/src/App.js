ead11f25ae0c1022695322701f6319825ab93df6
qmya <qandeelgul90@gmail.com>
2020-06-19 21:51:32 -0400

    cleanup and prettier the code

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,22 @@
-import React from 'react';
-// import icon from './assets/icons8-linkedin.svg';
-import Navigation from './components/Navigation/Navigation.js'
-import Home from './pages/Home/Home.js';
-import MyPlants from './pages/MyPlants/MyPlants.js';
-// import Footer from './components/Footer/Footer.js';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.scss';
+import React from "react";
+import Navigation from "./components/Navigation/Navigation.js";
+import Home from "./pages/Home/Home.js";
+import MyPlants from "./pages/MyPlants/MyPlants.js";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.scss";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navigation/>
+        <Navigation />
         <Switch>
-        <Route path='/' exact component ={Home} />;
-        <Route path='/myplants' component = {MyPlants}/>
+          <Route path="/" exact component={Home} />;
+          <Route path="/myplants" component={MyPlants} />
         </Switch>
-       
-        
       </BrowserRouter>
-      
     </>
-
-  ); 
+  );
 }
 
 export default App;
