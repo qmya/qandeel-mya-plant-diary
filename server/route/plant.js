3bec23df7e09f0e798cb683b9dc4a6d2e6e8ca40
qmya <qandeelgul90@gmail.com>
2020-06-19 20:48:48 -0400

    cleanup and prettier the code

--- server/route/plant.js
+++ server/route/plant.js
@@ -1,18 +1,15 @@
 const express = require("express");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const router = express.Router();
 const plantList = require("../data/plant.json");
-const userData = require ("../data/user.json");
-
+const userData = require("../data/user.json");
 
 router.get("/", (req, res) => {
- 
   res.status(200).json(userData);
 });
 
 router.get("/:id", (req, res) => {
-  
-  const plantid = userData.find((item) =>   item.id === req.params.id);
+  const plantid = userData.find((item) => item.id === req.params.id);
   if (plantid) {
     res.status(200).json(plantid);
   } else {
