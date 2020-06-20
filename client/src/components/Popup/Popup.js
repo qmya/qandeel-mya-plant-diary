580253c456ca586b42b59080c3287e7eaa749ef2
qmya <qandeelgul90@gmail.com>
2020-06-19 21:33:51 -0400

    cleanup and prettier the code

--- client/src/components/Popup/Popup.js
+++ client/src/components/Popup/Popup.js
@@ -1,24 +1,23 @@
-import React from 'react';  
-import './Popup.scss';  
+import React from "react";
+import "./Popup.scss";
 
-function Popup (props) {  
-   
-    
-
-  console.log(props)
-return (  
-    
-<div className='popup'>  
-<div className='popup__inner'> 
-<h1>Are you sure to delete this?</h1>  
-<div className='popup__flex'>
-<button className='popup__button'onClick={props.closePopup}>close me</button>  
-<button className='popup__button' onClick={props.deletePopup} >Yes please</button>  
-</div>
-</div>  
-</div>
-);  
-}  
- 
+function Popup(props) {
+  console.log(props);
+  return (
+    <div className="popup">
+      <div className="popup__inner">
+        <h1>Are you sure to delete this?</h1>
+        <div className="popup__flex">
+          <button className="popup__button" onClick={props.closePopup}>
+            close me
+          </button>
+          <button className="popup__button" onClick={props.deletePopup}>
+            Yes please
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Popup;
