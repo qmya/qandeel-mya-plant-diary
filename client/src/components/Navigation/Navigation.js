08bb48862c3bddf4b12b98bcbd9b9b0fd5e420fa
qmya <qandeelgul90@gmail.com>
2020-06-19 21:05:17 -0400

    cleanup and prettier the code

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./Navigation.scss";
-import { Link } from 'react-router-dom';
-import Logo from '../../assets/flower.svg';
+import { Link } from "react-router-dom";
+import Logo from "../../assets/flower.svg";
 
-function Navigation () {
-    
-    return ( 
-        <header className="header">
-            <div className="header__iconheader">
-            <Link to="/">
-             <img src ={Logo} className="header__icon" alt="logo for the plant site"/>
-            </Link>
-            <h1 className="header__one">Plant Diary</h1>
-            </div>
-            <nav>
-            
-                <Link to= "/myplants">
-                    <div className="header__two">My Plants</div> 
-                </Link>
-            </nav>
-        </header>
-    );
+function Navigation() {
+  return (
+    <header className="header">
+      <div className="header__iconheader">
+        <Link to="/">
+          <img
+            src={Logo}
+            className="header__icon"
+            alt="logo for the plant site"
+          />
+        </Link>
+        <h1 className="header__one">Plant Diary</h1>
+      </div>
+      <nav>
+        <Link to="/myplants">
+          <div className="header__two">My Plants</div>
+        </Link>
+      </nav>
+    </header>
+  );
 }
-export default Navigation;    
+export default Navigation;
