099e857eeba120f78b653ce1c7dc13d435ee8b00
qmya <qandeelgul90@gmail.com>
2020-06-19 21:21:05 -0400

    cleanup and prettier the code

--- client/src/components/PlantList/PlantList.js
+++ client/src/components/PlantList/PlantList.js
@@ -1,44 +1,40 @@
-import React from 'react';
-import PlantItem from '../PlantItem/PlantItem.js';
-import { v4 as uuidv4 } from 'uuid';
-import './PlantList.scss';
+import React from "react";
+import PlantItem from "../PlantItem/PlantItem.js";
+import { v4 as uuidv4 } from "uuid";
+import "./PlantList.scss";
 
-const PlantList =(props) => {
-    return(
-        <div className="plantlist" >
-            <div>
-                <h1 className="plantlist__header">Your Plants</h1>
-                <div  className="plantlist__flex">
-                    <p className="plantlist__paragraph">
-                        Below is the list of the plants you have added to your garden.</p>
-                    <button className="plantlist__button" onClick={props.openModal}>ADD PLANTS</button>
-                </div>
-                {props.plantList.length ? props.plantList.map(plant =>(
-                    
-                    <PlantItem 
-                    id={plant.id}
-                    key={uuidv4()}
-                    image={plant.image}
-                    name={plant.name} 
-                    nickName={plant.nickName}
-                    description={plant.description}
-                    light={plant.light}
-                    water={plant.water}
-                    waterfrequency={plant.waterfrequency}
-                    handleDelete={props.handleDelete}
-                    getData={props.getData}
-                    lastwatereddate={plant.lastWateredDate}
-                    // reminder={plant.last-watered-date}
-                    />
-                )): null}
-                
-            {/* </div> */}
-            
+const PlantList = (props) => {
+  return (
+    <div className="plantlist">
+      <div>
+        <h1 className="plantlist__header">Your Plants</h1>
+        <div className="plantlist__flex">
+          <p className="plantlist__paragraph">
+            Below is the list of the plants you have added to your garden.
+          </p>
+          <button className="plantlist__button" onClick={props.openModal}>
+            ADD PLANTS
+          </button>
         </div>
-        </div>
-    )
-}
+        {props.plantList.length
+          ? props.plantList.map((plant) => (
+              <PlantItem
+                id={plant.id}
+                key={uuidv4()}
+                image={plant.image}
+                name={plant.name}
+                nickName={plant.nickName}
+                description={plant.description}
+                light={plant.light}
+                water={plant.water}
+                handleDelete={props.handleDelete}
+                getData={props.getData}
+                lastwatereddate={plant.lastWateredDate}
+              />
+            ))
+          : null}
+      </div>
+    </div>
+  );
+};
 export default PlantList;
-
-
-
